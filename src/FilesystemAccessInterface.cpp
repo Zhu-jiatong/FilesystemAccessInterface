2561b1cbb131263c7d5742e42208ed04d2394131
Zhu-jiatong <zhujiatong@hotmail.com>
2023-10-30 00:19:38 +0000

    first commit

---
+++ src/FilesystemAccessInterface.cpp
@@ -0,0 +1,53 @@
+#include "FilesystemAccessInterface.h"
+#include "AccessPermissionManager/src/FilesystemUtility/src/FilesystemUtility.h"
+
+File FilesystemAccessInterface::openFile(const String &username, const String &path, const char *mode)
+{
+	Permission permission;
+	if (mode == FILE_READ)
+		permission = Permission::READ;
+	else if (mode == FILE_WRITE || mode == FILE_APPEND)
+		permission = Permission::WRITE;
+	else
+		throw std::runtime_error(("Invalid mode: " + String(mode)).c_str());
+
+	if (!canUserAccessFile(username, path, permission))
+		throw std::runtime_error((username + " does not have " + (permission == Permission::READ ? "read" : "write") + " access to " + path).c_str());
+
+	File file = _fs.open(path, mode);
+	if (!file)
+		throw std::runtime_error(("Failed to open file: " + path).c_str());
+
+	return file;
+}
+
+void FilesystemAccessInterface::deleteFile(const String &username, const String &path)
+{
+	if (!canUserAccessFile(username, path, Permission::WRITE))
+		throw std::runtime_error((username + " does not have delete access to " + path).c_str());
+
+	File root = _fs.open(path);
+	if (!root)
+		throw std::runtime_error(("Failed to open file: " + path).c_str());
+
+	FSUtil::recursiveDelete(_fs, root);
+}
+
+void FilesystemAccessInterface::renameFile(const String &username, const String &pathFrom, const String &pathTo)
+{
+	if (!canUserAccessFile(username, pathFrom, Permission::WRITE) && canUserAccessFile(username, pathTo, Permission::WRITE))
+		throw std::runtime_error((username + " does not have write access to " + pathFrom + " or " + pathTo).c_str());
+
+	if (!_fs.rename(pathFrom, pathTo))
+		throw std::runtime_error(("Failed to rename file: " + pathFrom + " to " + pathTo).c_str());
+}
+
+void FilesystemAccessInterface::createDirectory(const String &username, const String &path)
+{
+	String parent = FSUtil::parentPath(path);
+	if (!canUserAccessFile(username, parent, Permission::WRITE))
+		throw std::runtime_error((username + " does not have write access to " + parent).c_str());
+
+	if (!_fs.mkdir(path))
+		throw std::runtime_error(("Failed to create directory: " + path).c_str());
+}
---
+++ src/FilesystemAccessInterface.h
@@ -0,0 +1,22 @@
+#if !defined(_FilesystemAccessInterface_h_)
+#define _FilesystemAccessInterface_h
+
+#include <Arduino.h>
+#include <FS.h>
+#include "AccessPermissionManager/src/AccessPermissionManager.h"
+
+class FilesystemAccessInterface : public AccessPermissionManager
+{
+public:
+	FilesystemAccessInterface(FS &fs) : _fs(fs) {}
+
+	File openFile(const String &username, const String &path, const char *mode);
+	void deleteFile(const String &username, const String &path);
+	void renameFile(const String &username, const String &pathFrom, const String &pathTo);
+	void createDirectory(const String &username, const String &path);
+
+private:
+	FS &_fs;
+};
+
+#endif // _FilesystemAccessInterface_h
